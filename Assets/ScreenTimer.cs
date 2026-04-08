ad5fe87b03fcea0f0c4661f13e3899fe4ffbc83e
Seanuts <codyzhou53@gmail.com>
2026-04-08 09:55:56 -0400

    multi-slide-support
    Implemented automatic slide switching, interval currently shared for all slides

--- Assets/ScreenTimer.cs
+++ Assets/ScreenTimer.cs
@@ -1,40 +1,65 @@
 using UnityEngine;
 using TMPro;
 
-public class ScreenTimer : MonoBehaviour
-{
+public class ScreenTimer : MonoBehaviour{
     [Header("Timer Settings")]
     public TextMeshProUGUI timerText; 
-    public float timeRemaining = 60f; //60s
+    public float timePerScreen = 60f; //starting time of each new screen
+    private float timeRemaining;
     private bool timerIsRunning = false;
 
-    void Start()
-    {
-        timerIsRunning = true; 
-    }
+    [Header("Page Settings")]
+    public GameObject[] pages; 
+    private int currentPageIndex = 0; 
 
-    void Update()
-    {
-        if (timerIsRunning)
-        {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime; 
-                UpdateTimerDisplay(timeRemaining);
-            }
-            else
-            {
-                timeRemaining = 0;
-                timerIsRunning = false;
-                timerText.text = "00:00";
-                
-                Debug.Log("Time is up! Ready for next screen."); 
+    void Start(){
+        timeRemaining = timePerScreen;
+        timerIsRunning = true; 
+
+        for (int i = 0; i < pages.Length; i++){
+            if (pages[i] != null){
+                pages[i].SetActive(i == 0); //only sets to true if very first item (i= 0)
             }
         }
     }
 
-    void UpdateTimerDisplay(float timeToDisplay)
-    {
+    void Update(){
+        if (timerIsRunning){
+            if (timeRemaining > 0){
+                timeRemaining -= Time.deltaTime; 
+                UpdateTimerDisplay(timeRemaining);
+            }
+            else{
+                AdvanceToNextPage();
+            }
+        }
+    }
+
+    void AdvanceToNextPage(){
+        if (pages[currentPageIndex] != null){
+            pages[currentPageIndex].SetActive(false);
+        }
+
+        currentPageIndex++; 
+
+        if (currentPageIndex < pages.Length){ //still has pages?
+            if (pages[currentPageIndex] != null){//turn on new page
+
+                pages[currentPageIndex].SetActive(true);
+            }
+    
+            timeRemaining = timePerScreen;//reset timer
+            Debug.Log("Swapped to page: " + (currentPageIndex + 1));
+        }
+        else{
+            timeRemaining = 0;
+            timerIsRunning = false;
+            timerText.text = "00:00";
+            Debug.Log("Study Complete -> No more pages.");
+        }
+    }
+
+    void UpdateTimerDisplay(float timeToDisplay){
         timeToDisplay += 1; 
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
